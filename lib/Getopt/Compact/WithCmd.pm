508f07083cab483f423bb714587522bbadb4c8e0
xaicron <xaicron@gmail.com>
2010-09-12 04:55:06 +0900

    initial commit

---
+++ lib/Getopt/Compact/WithCmd.pm
@@ -0,0 +1,42 @@
+package Getopt::Compact::WithCmd;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Getopt::Compact::WithCmd -
+
+=head1 SYNOPSIS
+
+  use Getopt::Compact::WithCmd;
+
+=head1 DESCRIPTION
+
+Getopt::Compact::WithCmd is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2010 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
---
+++ xt/01_podspell.t
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Test::More;
+
+eval q{ use Test::Spelling; !system qw(which spell) or die };
+plan skip_all => "Test::Spelling and spell(1) are not available." if $@;
+
+add_stopwords(map { split /[\s\:\-]/ } <DATA>);
+$ENV{LANG} = 'C';
+all_pod_files_spelling_ok('lib');
+
+__DATA__
+Getopt::Compact::WithCmd
+
+# personal section
+xaicron
+xaicron {at} cpan.org
